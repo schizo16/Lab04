6c8d1886a42056fbc547eaf37644bef410c45528
Batman <Batman@DESKTOP-6V9DUQE>
2024-03-08 11:10:46 +0700

    Add project files.

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,133 @@
+﻿using Lab04.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab04.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: PeopleController
+        public ActionResult Index()
+        {
+            var peoples = DataLocal.GetPeople();
+            return View(peoples);
+        }
+
+        // GET: PeopleController/Details/5
+        public ActionResult Details(int id)
+        {
+            var peoples = DataLocal.GetPeopleById(id);
+            return View(peoples);
+        }
+
+        // GET: PeopleController/Create
+        public ActionResult Create()
+        {
+            People peole = new People();
+            return View(peole);
+        }
+
+        // POST: PeopleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(People model)
+        {
+            try
+            {
+                var files = HttpContext.Request.Form.Files;
+                if(files.Count() > 0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var FileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(),
+     "wwwroot\\images\\avatar", FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        model.Avatar = "/images/avatar/" + FileName;
+                    }
+                }
+                DataLocal.peoples.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ViewBag.error = ex.Message;
+                return View(model);
+            }
+        }
+
+        // GET: PeopleController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var people = DataLocal.GetPeopleById(id);
+            return View(people);
+        }
+
+        // POST: PeopleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, People model)
+        {
+            try
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count() > 0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var FileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(),
+     "wwwroot\\images\\avatar", FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        model.Avatar = "/images/avatar/" + FileName;
+                    }
+                }
+                for (int i = 0; i < DataLocal.peoples.Count; i++)
+                {
+                    if (DataLocal.peoples[i].Id == id)
+                    {
+                        DataLocal.peoples[i] = model;
+                        break;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: PeopleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var peoples = DataLocal.GetPeopleById(id);
+            return View(peoples);
+        }
+
+        // POST: PeopleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, People model)
+        {
+            try
+            {
+                for (int i = 0; i < DataLocal.peoples.Count; i++)
+                {
+                    if (DataLocal.peoples[i].Id == id)
+                    {
+                        DataLocal.peoples.RemoveAt(i);
+                        break;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/DataLocal.cs
@@ -0,0 +1,78 @@
+﻿namespace Lab04.Models
+{
+    public class DataLocal
+    {
+        public static List<People> peoples = new List<People>()
+        {
+            new People()
+            {
+                Id = 0,
+                Name = "Devmaster",
+                Email = "devmaster.edu.vn@gmail.com",
+                Phone = "1234567890",
+                Address = "Ha Noi",
+                Avatar = "/images/avatar/01.jpg",
+                Birthday= Convert.ToDateTime("2012/09/22"),
+                Bio = "Vien cong nghe Devmaster",
+                Gender =2
+            },
+            new People()
+            {
+                Id = 1,
+                Name = "TuanAnh",
+                Email = "tuananh@gmail.com",
+                Phone = "1234567890",
+                Address = "Ha Noi",
+                Avatar = "/images/avatar/02.jpg",
+                Birthday= Convert.ToDateTime("2002/09/22"),
+                Bio = "Vien cong nghe Devmaster",
+                Gender =1
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Guts",
+                Email = "devmaster.edu.vn@gmail.com",
+                Phone = "1234567890",
+                Address = "Ha Noi",
+                Avatar = "/images/avatar/03.jpg",
+                Birthday= Convert.ToDateTime("1994/09/22"),
+                Bio = "Vien cong nghe Devmaster",
+                Gender =2
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Doremon",
+                Email = "devmaster.edu.vn@gmail.com",
+                Phone = "1234567890",
+                Address = "Ha Noi",
+                Avatar = "/images/avatar/04.jpg",
+                Birthday= Convert.ToDateTime("1984/09/22"),
+                Bio = "Vien cong nghe Devmaster",
+                Gender =1
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Pikachu",
+                Email = "devmaster.edu.vn@gmail.com",
+                Phone = "1234567890",
+                Address = "Ha Noi",
+                Avatar = "/images/avatar/05.jpg",
+                Birthday= Convert.ToDateTime("1985/09/22"),
+                Bio = "Vien cong nghe Devmaster",
+                Gender =2
+            },
+        };
+        public static List<People> GetPeople()
+        {
+            return peoples;
+        }
+        public static People? GetPeopleById(int Id)
+        {
+            var people = peoples.FirstOrDefault(x => x.Id == Id);
+            return people;
+        }
+    }
+}
